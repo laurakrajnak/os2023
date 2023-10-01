112bb5b7a3346454feefb9fee091a371c9573d81
laurakrajnak <laura.lk40@gmail.com>
2023-10-01 16:12:22 +0200

    sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,1 +1,19 @@
-xchxhsdhsdhshs
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int ticks;
+
+  if (argc != 2) {
+    fprintf92, "sleep usage: sleep <ticks>\n");
+  }
+  ticks = atoi(argv[1]);
+
+  sleep(ticks);
+
+  exit(0);
+
+}
