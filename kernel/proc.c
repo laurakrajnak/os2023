90a1f041361b1501fa66e618e7aef9249cd97d2b
laurakrajnak <laura.lk40@gmail.com>
2023-10-03 19:57:04 +0200

    sysinfo trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -707,17 +707,3 @@ uint64 cal_nproc(){
   }
   return nproc;
 }
-
-uint64 sys_sysinfo(void){
-  struct sysinfo kinfo;
-  uint64 info;
-  if(argaddr(0,&info)<0)
-    return -1;
-  kinfo.freemem = cal_freemem();
-  kinfo.nproc = cal_nproc();
-  if(copyout(myproc()->pagetable,(uint64)info,(char*)&kinfo,sizeof(struct sysinfo))<0){
-    return -1;
-  }
-  return 0;
-}
-
