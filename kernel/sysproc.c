cfa656273713b7eb6a571475d56b1ae4609fde89
laurakrajnak <laura.lk40@gmail.com>
2023-10-04 14:39:38 +0200

    additional changes

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -14,7 +14,7 @@ sys_exit(void)
   argint(0, &n);
   exit(n);
   return 0;  // not reached
-}
+ }
 
 uint64
 sys_getpid(void)
@@ -93,25 +93,65 @@ sys_uptime(void)
   return xticks;
 }
 
+
+//makefile
+//user/user.h
+//user/usys.pl
+//kernel/syscall.h
+//pridat sys_trace/sys_info do kernel/sysproc.c
+
+
+//uprav fork() v kernel/proc.c
+//uprav syscall() kernel/syscall.c
+
 // trace
 uint64
 sys_trace(void){
   int n;
-  argint(0, &n);  
+
+  // Extract the integer argument from the system call using argint.
+  // Argument 0 refers to the first argument passed to the system call.
+  argint(0, &n);
+
+  // Set the trace mask for the current process.
+  // The trace mask is a way to enable or disable tracing for specific system calls.
   myproc()->mask = n;
+
+  // Return 0 to indicate that the system call has completed successfully.
   return 0;
 }
+
+
+
+//cal_freemem v kernel/kalloc.c
+//cal_nproc v kernel/proc.c
+//pridat do defs.h
 
 uint64
 sys_sysinfo(void){
-
+  // Declare a struct to store system information, and a variable to hold the user-provided pointer.
   struct sysinfo kinfo;
   uint64 info;
-  argaddr(0,&info);
+
+  // Extract the user-provided pointer (argument 0) from the system call.
+  argaddr(0, &info);
+
+  // Calculate and populate system information into the 'kinfo' structure.
+  // 1. Calculate the amount of free memory in the system using 'cal_freemem'.
   kinfo.freemem = cal_freemem();
+  
+  // 2. Calculate the number of active processes using 'cal_nproc'.
   kinfo.nproc = cal_nproc();
-  if(copyout(myproc()->pagetable,(uint64)info,(char*)&kinfo,sizeof(struct sysinfo))<0){
+  
+  // Copy the 'kinfo' structure to the user-provided memory using 'copyout'.
+  // 'copyout' ensures that the kernel can safely copy data to the user's address space.
+  if (copyout(myproc()->pagetable, (uint64)info, (char*)&kinfo, sizeof(struct sysinfo)) < 0) {
+    // If the copy operation fails, return an error code (-1).
     return -1;
   }
+
+  // Return 0 to indicate that the system call has completed successfully.
   return 0;
 }
+
+
